8d3f8ff06e0299d848d5c97f1c69890627ed2926
James Boswell <jamesboswell@Jamess-MacBook-Pro.local>
2023-08-09 19:52:36 -0400

    render images in details modal

--- client/components/Modal.js
+++ client/components/Modal.js
@@ -9,9 +9,23 @@ const Modal = ({ handleClose, show, rowObj }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
   return (
-    <div className={showHideClassName}>
-      <section className="modal-main">
-        <PlaceDetails name = {rowObj.place_name}></PlaceDetails>
+    <div
+    className={showHideClassName}
+    style={{
+      position: 'fixed',
+      backgroundColor: 'white',
+      border: '1px solid #ccc',
+      borderRadius: '5px',
+      padding: '20px',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      maxHeight: '80%', // Set a maximum height for the modal
+      overflowY: 'auto', // Enable vertical scrolling when content exceeds max-height
+    }}
+  >
+      <section className="modal-main" >
+        <PlaceDetails name = {rowObj.place_name} category = {rowObj.category} address = {rowObj.address} show = {show}></PlaceDetails>
         <button type="button" onClick={handleClose}>
           Close
         </button>
--- client/components/ResultRow.jsx
+++ client/components/ResultRow.jsx
@@ -29,6 +29,7 @@ const ResultRow = (props) => {
     //add place id to user document saved array
     const [show, setShow] = useState({show : false})
     const detailsClickHandle = () =>{
+
         showModal()
     }
 
@@ -44,12 +45,18 @@ const ResultRow = (props) => {
         <tr>
             <td>{place_name}</td>
             <td>{address}</td>
+            <td>
             <button>Rate</button>
             <button onClick={savePlaceFunc}>Save</button>
             <Modal show = {show.show} handleClose = {hideModal} rowObj = {props.result}></Modal>
-            <button onClick = {detailsClickHandle}>Details</button>
+            <button id = "setModal" onClick = {detailsClickHandle}>Details</button>
+            </td>
+            {show && (
+                <Modal show={show.show} handleClose={hideModal} rowObj={props.result} />
+            )}
         </tr>
     );
 }
+//             <Modal show = {show.show} handleClose = {hideModal} rowObj = {props.result}></Modal>
 
 export default ResultRow;
